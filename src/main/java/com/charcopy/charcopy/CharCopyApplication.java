1509531c6c80e94d347bc85a1eb5598c91a4677f
dariofedes <dariofedes@protonmail.com>
2021-09-20 11:01:34 +0200

    initial commit

---
+++ src/main/java/com/charcopy/charcopy/CharCopyApplication.java
@@ -0,0 +1,13 @@
+package com.charcopy.charcopy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CharCopyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CharCopyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/charcopy/charcopy/CharCopyApplicationTests.java
@@ -0,0 +1,14 @@
+
+package com.charcopy.charcopy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CharCopyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
